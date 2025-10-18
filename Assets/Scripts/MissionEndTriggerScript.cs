322ca90b6ffbc50883739374004f4b8c347df742
mrose-csu <maximilian.rose@colostate.edu>
2025-10-18 00:50:04 -0600

    Added some more mission logic. Now you have to stop inside of the trigger zones to start/stop missions.

--- Assets/Scripts/MissionEndTriggerScript.cs
+++ Assets/Scripts/MissionEndTriggerScript.cs
@@ -4,14 +4,27 @@ public class MissionEndTriggerScript : MonoBehaviour
 {
 
     public MissionLogic missionLogic;
+    public float stopThreshold = 0.1f;
+    public float holdTime = 0.5f;
+
+    private StopMonitor stopMonitor;
 
     // Called once when another collider enters this trigger
     private void OnTriggerEnter(Collider other)
     {
         // Example: check tag
-        if (other.CompareTag("Car"))
+        if (other.CompareTag("PlayerCar"))
         {
-            missionLogic.EndMission(other);
+            if (stopMonitor == null) stopMonitor = GetComponentInChildren<StopMonitor>() ?? gameObject.AddComponent<StopMonitor>();
+            stopMonitor.StartMonitoring(other, stopThreshold, holdTime, (c) => missionLogic.EndMission(c));
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("PlayerCar"))
+        {
+            stopMonitor?.StopMonitoring(other);
         }
     }
 }
--- Assets/Scripts/MissionLogic.cs
+++ Assets/Scripts/MissionLogic.cs
@@ -6,28 +6,31 @@ public class MissionLogic : MonoBehaviour
 {
 
     public TimerController timerController;
+    public TextMeshProUGUI missionText;
 
     public void StartMission(Collider other)
     {
         timerController.StopTimer();
         timerController.StartTimer();
+        missionText.text = "To the liquor sto'!";
     }
 
-    
+
     public void EndMission(Collider other)
     {
         timerController.PauseTimer();
+        missionText.text = "";
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
