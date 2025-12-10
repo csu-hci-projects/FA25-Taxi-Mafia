264d431bbd2a9e95a6b63e4cd0946043e1c70c5b
Stucke03 <nathan@rot26.net>
2025-12-09 22:57:28 -0700

    3 new colors and a voice clip for not enough funds

--- Assets/Scripts/ColorTaxi.cs
+++ Assets/Scripts/ColorTaxi.cs
@@ -10,17 +10,24 @@ public class ColorTaxi : MonoBehaviour
     public Material blueMaterial;
     public Material yellowMaterial;
     public Material copMaterial;
+    public Material pinkMaterial;
+    public Material brownMaterial;
+    public Material greenMaterial;
 
     [Header("Costs")]
     public int redCost = 100;
     public int blueCost = 200;
     public int yellowCost = 50;
     public int copCost = 300;
+    public int pinkCost = 200;
+    public int brownCost = 200;
+    public int greenCost = 200;
 
     [Header("Money System")]
     public MissionLogic missionLogic;  // <-- reference your existing money script
 
     private Material _currentMaterial;
+    public AudioSource notEnoughMoney;
 
     void Start()
     {
@@ -46,6 +53,18 @@ public class ColorTaxi : MonoBehaviour
     {
         TryChangeColor(copMaterial, copCost);
     }
+    public void ChangeToPink()
+    {
+        TryChangeColor(pinkMaterial, pinkCost);
+    }
+    public void ChangeToBrown()
+    {
+        TryChangeColor(brownMaterial, brownCost);
+    }
+    public void ChangeToGreen()
+    {
+        TryChangeColor(greenMaterial, greenCost);
+    }
 
     private void TryChangeColor(Material newMaterial, int cost)
     {
@@ -58,6 +77,7 @@ public class ColorTaxi : MonoBehaviour
         if (money < cost)
         {
             Debug.Log("Not enough money to change color.");
+            notEnoughMoney.Play();
             return;
         }
 
